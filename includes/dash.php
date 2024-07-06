5e1a9bdfdd25d2ff07f2392aed7d6b3855087e60
d k <dkovi24@gmail.com>
2024-07-06 19:35:28 -0400

    Removal and Fixes

--- includes/dash.php
+++ includes/dash.php
@@ -1,20 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Dashboard</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+        <title>Dashboard</title>
+         <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
     <div class="container">
         <?php
-        // Connect to the database
-        $servername = "localhost";
-        $username = "username";
-        $password = "password";
-        $dbname = "main";
-
-        $conn = new mysqli($servername, $username, $password, $dbname);
-
         // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
