773ae760e040292a4d3e8b7d1626fc276320c5ce
d k <dkovi24@gmail.com>
2024-07-07 03:10:57 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -8,13 +8,14 @@ $_SESSION = array();
 
 //If it's desired to kill the bitch, then just let it kill itself
 //PS this will kill the session :)))) and not just the session data :)))
-if (ini_get("session.use_cookies")) {
+if (ini_get("session.use_cookies")) 
+{
     $params = session_get_cookie_params();
         setcookie(session_name(), '', time() - 42000,
             $params["path"], $params["domain"],
             $params["secure"], $params["httponly"]
         );
-    }
+}
 
 //Destroy the session
 session_destroy();
