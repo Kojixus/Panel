ae018d514edd25455c79975e853a1cd553b8ee18
d k <dkovi24@gmail.com>
2024-07-08 00:33:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,16 @@
     <title>Homepage</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="scripts/background.js"></script>
-    <link rel="icon" type="img/png" href="imgs/kr_favicon.png">
+    <link rel="icon" type="img/png" href="img/kr_favicon.png">
 </head>
 <div class="taskbar">
     <ul>
         <li><a href="login.php">Login</a></li>
         <li><a href="signup.php">Signup</a></li>
     </ul>
+</div>
+<div class="container">
+    <img src="img/kr.jpg" alt="Centered Image" class="centered-image">
 </div>
 </html>
 
